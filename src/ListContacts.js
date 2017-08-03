433669320e70dc8848555868c78b7152c3626ff5
Josh Harris <joshua.harris1085@gmail.com>
2017-08-03 12:36:04 -0500

    Change ListContacts to stateless component

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,25 +1,23 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-class ListContacts extends Component {
-  render() {
-    return (
-      <ol className='contact-list'>
-        {this.props.contacts.map((contact) => (
-            <li className='contact-list-item' key={contact.id}>
-              <div className='contact-avatar' style={{
-                backgroundImage: `url(${contact.avatarURL})`
-              }}/>
-              <div className='contact-details'>
-                <p>{contact.name}</p>
-                <p>{contact.email}</p>
-              </div>
-              <button className='contact-remove' onClick={() => this.props.onDeleteContact(contact)}>Remove</button>
-            </li>
-        ))}
-      </ol>
-    )
-  }
+function ListContacts(props) {
+  return (
+    <ol className='contact-list'>
+      {props.contacts.map((contact) => (
+          <li className='contact-list-item' key={contact.id}>
+            <div className='contact-avatar' style={{
+              backgroundImage: `url(${contact.avatarURL})`
+            }}/>
+            <div className='contact-details'>
+              <p>{contact.name}</p>
+              <p>{contact.email}</p>
+            </div>
+            <button className='contact-remove' onClick={() => props.onDeleteContact(contact)}>Remove</button>
+          </li>
+      ))}
+    </ol>
+  )
 }
 
 ListContacts.propTypes = {
