78cf963c56c4a3d34d349bfd1176b0b6eb76d855
Josh Harris <joshua.harris1085@gmail.com>
2017-08-07 00:05:06 -0500

    Add ability to create user

--- src/App.js
+++ src/App.js
@@ -8,11 +8,13 @@ class App extends Component {
   state = {
     contacts: []
   }
+
   componentDidMount() {
     ContactsAPI.getAll().then((contacts) => {
       this.setState({ contacts })
     })
   }
+
   removeContact = (contact) => {
     this.setState((state) => ({
       contacts: state.contacts.filter((c) => c.id !== contact.id)
@@ -20,16 +22,35 @@ class App extends Component {
 
     ContactsAPI.remove(contact)
   }
+
+  createContact(contact) {
+    ContactsAPI.create(contact).then(contact => {
+      this.setState(state => ({
+        contacts: state.contacts.concat([contact])
+      }))
+    })
+  }
+
   render() {
     return (
       <div>
+
         <Route exact path='/' render={() => (
           <ListContacts
             contacts={this.state.contacts}
             onDeleteContact={this.removeContact}
           />
         )}/>
-        <Route path='/create' component={CreateContact}/>
+
+        <Route path='/create' render={({ history }) => (
+          <CreateContact
+          onCreateContact={(contact) => {
+            this.createContact(contact)
+            history.push('/')
+          }}
+          />
+        )}/>
+
       </div>
     )
   }
